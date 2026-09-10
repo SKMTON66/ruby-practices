3be75887bf4fe9e3af1ab974b9f9fd55791df70c
TONGARARIN <tongararin@gmail.com>
2026-09-10 15:44:28 +0900

    shot_testの不要な空白を削除

--- 07.bowling_object/test/shot_test.rb
+++ 07.bowling_object/test/shot_test.rb
@@ -1,21 +1,20 @@
-  # frozen_string_literal: true
+# frozen_string_literal: true
 
-  require 'minitest/autorun'
-  require_relative '../lib/shot'
-
-  class ShotTest < Minitest::Test
-    def setup
-      @shot = Shot.new('9')
-      @strike_shot = Shot.new('X')
-    end
-
-    def test_shot_score
-      assert_equal 9, @shot.score
-      assert_equal 10, @strike_shot.score
-    end
-
-    def test_strike
-      assert @strike_shot.strike?
-      refute @shot.strike?
-    end
+require 'minitest/autorun'
+require_relative '../lib/shot'
+class ShotTest < Minitest::Test
+  def setup
+    @shot = Shot.new('9')
+    @strike_shot = Shot.new('X')
   end
+
+  def test_shot_score
+    assert_equal 9, @shot.score
+    assert_equal 10, @strike_shot.score
+  end
+
+  def test_strike
+    assert @strike_shot.strike?
+    refute @shot.strike?
+  end
+end
